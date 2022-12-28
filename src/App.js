1747f43b68f20f867373552c02e6b2b20169205b
valderez <valderezpe@gmail.com>
2022-12-28 16:11:41 -0300

    conectando aplicação com Api-Binance tempo real.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import { useEffect,useState } from "react";
 import Chart from "./Chart";
+import { getCandles } from "./DataService";
 
 function App() {
+
+  const [symbol, setSymbol]= useState('BTCUSDT');
+  const [interval, setInterval]= useState('1m');
+
+  const [data, setData] = useState([]);
+
+  useEffect(() =>{
+          
+    getCandles(symbol, interval)
+    .then(data => setData(data))
+    .catch(err => alert(err.response ? err.response.data : err.message))
+  }, [symbol, interval])
+
+  function onSymbolChange(event) {
+    setSymbol(event.target.value)
+  }
+
+  function onIntervalChange(event) {
+    setInterval(event.target.value)
+  }
+
   return (
    <div>
-    <Chart/>
+    <select onChange={onSymbolChange}>
+      <option>BTCUSDT</option>
+      <option>ETHUSDT</option>
+      <option>ADAUSDT</option>
+    </select>
+    <select onChange={onIntervalChange}>
+      <option>1m</option>
+      <option>1d</option>
+      <option>1w</option>
+    </select>
+    <Chart data = {data}
+    />
    </div>
   );
 }
